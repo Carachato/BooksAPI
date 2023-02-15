7d1c1def427bedbda862df84a3137fa67d75794b
CaraChato <carachatoqru@hotmail.com>
2023-02-15 20:39:09 -0300

    First commit

---
+++ BooksAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using BooksAPI.Model;
+
+namespace BooksAPI.Repositories
+{
+    public interface IBookRepository 
+    {
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(int Id);
+
+        Task<Book> Create(Book book);
+
+        Task Update(Book book);
+
+        Task Delete(int Id);
+    }
+}
